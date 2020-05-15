6277053fede749bce8371ba258d689f62e423563
JorgeRod2594 <jor567.jr@gmail.com>
2020-05-15 12:47:03 -0500

    Crea state y funciones para nuevos proyectos

--- cliente/src/components/proyectos/NuevoProyecto.jsx
+++ cliente/src/components/proyectos/NuevoProyecto.jsx
@@ -1,6 +1,40 @@
-import React, {Fragment} from 'react'
+import React, {Fragment, useState} from 'react'
 
 const NuevoProyecto = () => {
+
+    //Definimos el sate de este componente
+    //Creamos un objeto para asociarlo con un id y no tener problemas
+    //para identificarlo o borrarlo.
+    const [proyecto, guardarProyecto] = useState({
+        nombre:''
+    });
+
+    //Aplicamos destructuring y extraemos el nombre del proyecto
+    const {nombre} = proyecto;
+
+    //Lee los contenidos de proyecto
+    //esta funcion la agregamos al input donde el usuario escribe
+    const onChangeProyecto = (e) => {
+        guardarProyecto({
+            ...proyecto,//tomamos una copia del proyecto
+            [e.target.name] : e.target.value //le asignamos el valor
+        });
+    }
+
+    //Cuando el usuario envia un proyecto nuevo
+    const onSubmitProyecto = (e) => {
+        //PreventDefault para que no haga la accion default y nos permita
+        //hacer las operaciones que necesitemos
+        e.preventDefault();
+
+        //Validar los datos
+
+        //Agregar al state
+
+        //reainiciar el form
+
+    }
+
     //lo importamos al Sidebar donde se utilizará.
     return ( 
         <Fragment>
@@ -12,12 +46,15 @@ const NuevoProyecto = () => {
 
             <form
                 className="formulario-nuevo-proyecto"
+                onSubmit={onSubmitProyecto}
             >
                 <input 
                 type="text"
                 className="input-text"
                 placeholder="Nombre del proyecto"
-                name="nombreproy"/>
+                onChange={onChangeProyecto}
+                value={nombre}
+                name="nombre"/>
 
                 <input 
                 type="submit"
