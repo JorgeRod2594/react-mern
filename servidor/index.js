0ab1090ce3a72eeda7a856abc94c7a6ba45c6e73
JorgeRod2594 <jor567.jr@gmail.com>
2020-05-29 13:48:17 -0500

    Creacion de el servidor express

--- servidor/index.js
+++ servidor/index.js
@@ -1,1 +1,17 @@
-console.log('Saludos desde el index...');
+//De momento node no soporta los imports por que definimos el servidor express de la siguiente manera:
+const express = require('express');
+
+//Creamos el servidor. Estaremos utilizando lo que se conocer como midleware
+const app = express();
+//Creamos el puerto para el servidor
+const PORT = process.env.PORT || 4000;
+
+//Definimos la pagina principal
+app.get('/', (req, res) => {
+    res.send('Hola mundo..')
+})
+
+//Arrancamos el servidor y mandara un mensaje con el puerto de conexion
+app.listen(PORT, () => {
+    console.log(`El servidor esta funcionando en el puerto ${PORT}`);
+})
