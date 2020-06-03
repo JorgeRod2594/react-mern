a97d0573db71173a7b58cf4b8a706196606edff5
JorgeRod2594 <jor567.jr@gmail.com>
2020-06-03 14:14:40 -0500

    Funcion en el controller para insertar nuevos proyectos

--- servidor/controllers/proyectoController.js
+++ servidor/controllers/proyectoController.js
@@ -1,3 +1,20 @@
-exports.crearProyecto = (req, res) => {
-    
+const Proyecto = require('../models/Proyecto');
+const { validationResult } = require('express-validator');
+
+exports.crearProyecto = async(req, res) => {
+
+    try {
+
+        //Creamos un nuevo proyecto
+        const proyecto = new Proyecto(req.body);
+        proyecto.save();
+
+        //Cuando todo este completo mandamos el nuevo proyecto creado
+        res.json(proyecto);
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
+
 }
