3ebe6bc489999089dda8af1c53f88f43beb8a69e
JorgeRod2594 <jor567.jr@gmail.com>
2020-06-15 16:41:51 -0500

    Validando que no existan espacios vacios y mostrando alertas

--- cliente/src/components/auth/NuevaCuenta.jsx
+++ cliente/src/components/auth/NuevaCuenta.jsx
@@ -1,48 +1,58 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import {Link} from 'react-router-dom'
+import AlertaContext from '../../context/alertas/alertaContext';
 
 const NuevaCuenta = () => {
 
+    //Extraemos los valores del context
+    const alertaContext = useContext(AlertaContext);
+    const {alerta, mostrarAlerta} = alertaContext
 
-//Creamos el state que para este componente. Nos permite guardar los datos de sesion
-const [nusuario, guardarUsuario] = useState({
-    nombre:'',
-    email:'',
-    password:'',
-    confpassword:''
-});
+    //Creamos el state que para este componente. Nos permite guardar los datos de sesion
+    const [nusuario, guardarUsuario] = useState({
+        nombre:'',
+        email:'',
+        password:'',
+        confpassword:''
+    });
 
-//Extraemos de usuario
-const {nombre, email, password, cpassword} = nusuario;
+    //Extraemos de usuario
+    const {nombre, email, password, cpassword} = nusuario;
 
-//declaramos la funcion que se estará ejecutando cada vez que el usuario escriba algo.
-const onChangeLogin = (e) => {
-    guardarUsuario({
-        /*Tomamos una copia de usuario, lo que este en usuario para que no
-        re escriba lo que esta en el otro campo en el state*/
-        ...nusuario, /*y re escrbimos la copia con */
-        [e.target.name] : e.target.value 
-        /*le pasamos por medio del nombre del input el valor del mismo*/
-    })
-}
-//Validaremos con express que tenga un email valido
-//Cuando el usuario quiere iniciar sesion
-const onSubmitLogin = (e) => {
-    e.preventDefault();
+    //declaramos la funcion que se estará ejecutando cada vez que el usuario escriba algo.
+    const onChangeLogin = (e) => {
+        guardarUsuario({
+            /*Tomamos una copia de usuario, lo que este en usuario para que no
+            re escriba lo que esta en el otro campo en el state*/
+            ...nusuario, /*y re escrbimos la copia con */
+            [e.target.name] : e.target.value 
+            /*le pasamos por medio del nombre del input el valor del mismo*/
+        })
+    }
+    //Validaremos con express que tenga un email valido
+    //Cuando el usuario quiere iniciar sesion
+    const onSubmitLogin = (e) => {
+        e.preventDefault();
 
-    //Validar que no exitan campos vacios
+        //Validar que no exitan campos vacios
+        if( nombre.trim() === '' || email.trim() === '' ||
+            password.trim() === '' || cpassword.trim() === ''){
+                return mostrarAlerta('Todos los campos son obligatorios','error');
+            }
 
-    //Validar password (mínimo 6 caracteres).
+        //Validar password (mínimo 6 caracteres).
 
-    //Validar que los dos password sean iguales.
+        //Validar que los dos password sean iguales.
 
-    //Pasarlo al action
+        //Pasarlo al action
 
-}
+    }
 
     return ( 
         <div className="form-usuario">
+            
             <div className="contenedor-form sombra-dark">
+                
                 <h1>Crear cuenta</h1>
 
                 <form
@@ -87,6 +97,8 @@ const onSubmitLogin = (e) => {
                         placeholder="Confirmar password"
                         onChange={onChangeLogin}/>
                     </div>
+
+                    {alerta ? (<div className={`alerta zum ${alerta.categoria}`}>{alerta.msg}</div>): null}
 
                     <div className="campo-form">
                         <input type="submit" className="btn btn-primario btn-block" value="Iniciar Sesion"/>
